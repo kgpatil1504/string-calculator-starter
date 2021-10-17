cd0672a5a6c27261faa8b7bf9e2e1f55977454b8
Kirti <kirtipatil1504@gmail.com>
2021-10-17 21:27:38 +0530

    Done with all test cases

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,9 +1,46 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+    	if(input.isEmpty())
+            return 0;
+        	else {
+        		String[] numbers = number_extractor(input);
+        		int result=0;
+        		StringBuilder negatives= new StringBuilder();
+        		for(int i=0; i<numbers.length; i++ ) {
+        			if(Integer.parseInt(numbers[i])<0)
+        				negatives.append(Integer.parseInt(numbers[i]));
+        		}
+        		if(negatives.length()==0) {
+        		for(int i=0; i<numbers.length; i++ ) {
+        			result +=Integer.parseInt(numbers[i]);
+        		}
+        		return result;
+        		}
+        		else {
+        			throw new RuntimeException("No negatives allowed and negative numbers are"+negatives);
+        		}
+         	}
+        	
+        }
+        
+        private static String[] number_extractor(String input) {
+        	if(input.startsWith("//")) {
+        		Matcher m=Pattern.compile("//(.)\n(.*)").matcher(input);
+        		m.matches();
+        		String customdel = m.group(1);
+        		String numbers = m.group(2);
+        		return numbers.split(customdel);
+        		}
+        	else {
+        	String[] numbers = input.split(",|\n");
+        	return numbers;
+        }
+        }
     }
 
-}
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
 
-    @Test
+	@Test
     void empty_string_should_return_0() {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(""));
@@ -17,4 +17,30 @@ class StringCalculatorShould {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
     }
+    
+    @Test
+    void string_with_multi_numbers_should_return_their_sum_as_int() {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(4, stringCalculator.add("2,2"));
+    }
+    
+    @Test
+    void string_with_newline_between_numbers_should_return_their_sum_as_int() {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(8, stringCalculator.add("2,2\n4"));
+    }
+    
+    @Test
+    void string_withcustom_delimeterandnewline_numbers_should_return_their_sum_as_int() {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(3, stringCalculator.add("//;\n1;2"));
+    }
+    
+    @Test
+    void string_withnegatives_should_throwException() {
+        StringCalculator stringCalculator = new StringCalculator();
+        stringCalculator.add("1,-2");
+    }
+    
+    
 }
